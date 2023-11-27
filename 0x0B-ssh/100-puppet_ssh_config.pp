b0b827ea9ab5e07abc83d1dadb45ba8289b3c729
Thabisa <tab95mazingisa@gmail.com>
2023-11-27 03:01:43 -0800

    100-puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#make changes to the config using puppet
+
+file { 'etc/ssh/ssh_cofig':
+ensure => present,
+content =>"
+#Configuration
+host*
+IdentityFile ~/.ssh/school
+PasswordAuthentication no
+}
